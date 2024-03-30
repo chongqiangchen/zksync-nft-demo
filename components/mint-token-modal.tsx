a499a91eed2049959863405b5689b5630e99447e
chenchongqiang <chongqiangchen@gmail.com>
2024-03-31 06:28:54 +0800

    feat: complete

--- components/mint-token-modal.tsx
+++ components/mint-token-modal.tsx
@@ -4,10 +4,9 @@ import { TOKEN_ADDRESS } from "@/constants/contract";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
 import useToken from "@/hooks/use-token";
 import { useQuery } from "react-query";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { Button } from "./ui/button";
 import Checkout from "./checkout";
-import { ethers } from "ethers";
 
 const MintTokenModal = () => {
     const [openModal, setOpenModal] = useState<boolean>(false);
@@ -16,6 +15,7 @@ const MintTokenModal = () => {
         getTokenMintEstimate,
         mint,
         isMintLoading,
+        canNonGas,
         isAllowancePaymaster,
         approvePaymaster,
         isApprovePaymasterLoading
@@ -28,11 +28,6 @@ const MintTokenModal = () => {
         enabled: openModal
     })
 
-    const nonGas = useMemo(
-        () => tokenBalance ? ethers.parseEther(tokenBalance!) > ethers.parseEther("1") : false,
-        [tokenBalance]
-    )
-
     return (
         <Dialog open={openModal} onOpenChange={setOpenModal}>
             <DialogTrigger>
@@ -40,7 +35,7 @@ const MintTokenModal = () => {
             </DialogTrigger>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>领取支付代币 {nonGas && "(无GAS版)"}</DialogTitle>
+                    <DialogTitle>领取支付代币 {canNonGas && "(无GAS版)"}</DialogTitle>
                     <DialogDescription>用于支付手续费的代币</DialogDescription>
                 </DialogHeader>
                 <div className="flex flex-col gap-4 mb-4">
@@ -50,26 +45,28 @@ const MintTokenModal = () => {
                         gas={tokenMintEstimate?.gas}
                         gasPrice={tokenMintEstimate?.gasPrice}
                         cost={tokenMintEstimate?.cost}
-                        nonGas={nonGas}
+                        nonGas={canNonGas}
                         transaction="Mint (amount = 1000 WTF)"
                     />
                 </div>
                 <DialogFooter>
-                    {!isAllowancePaymaster ? (
-                        <Button
-                            size="sm"
-                            className="w-full"
-                            disabled={isApprovePaymasterLoading}
-                            onClick={() => approvePaymaster()}
-                        >Approve Paymaster</Button>
-                    ) : (
-                        <Button
-                            size="sm"
-                            className="w-full"
-                            disabled={isMintLoading}
-                            onClick={() => mint()}
-                        >开始执行</Button>
-                    )}
+                    {
+                        isAllowancePaymaster ? (
+                            <Button
+                                size="sm"
+                                className="w-full"
+                                disabled={isMintLoading}
+                                onClick={() => mint()}
+                            >开始执行</Button>
+                        ) : (
+                            <Button
+                                size="sm"
+                                className="w-full"
+                                disabled={isApprovePaymasterLoading}
+                                onClick={() => approvePaymaster()}
+                            >授权代币作为手续费支付</Button>
+                        )
+                    }
                 </DialogFooter>
             </DialogContent>
         </Dialog>
