4eaba2d5382b9a0f02f439d3f7ffdfb36e5d2cf0
chenchongqiang <chongqiangchen@gmail.com>
2024-03-30 18:22:54 +0800

    feat: add basic page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image";
+"use client";
+
 import ConnectButton from "@/components/connect-button";
+import {useState} from "react";
+
+enum EStep {
+    CONNECT,
+    MINT
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ConnectButton />
-    </main>
-  );
+    const [step, setStep] = useState(EStep.MINT);
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            {step === EStep.CONNECT && (
+                <div className="px-10 py-8 bg-[#ffffff] rounded-lg shadow">
+                    STEP1 CONNECT WALLET
+                </div>
+            )}
+            {step === EStep.MINT && (
+                <div className="px-10 py-8 bg-[#1E1E1E] rounded-lg shadow text-[#ffffff]">
+                    STEP2 MINT NFT
+                </div>
+            )}
+        </main>
+    );
 }
